4eca8e2ab7690c5a93f0a7621ecf7b399d77dd28
MaharshiChetan <chetanmahabboy@gmail.com>
2018-09-17 23:44:10 +0530

    fetching 10 users per scroll

--- src/pages/interested-or-going/interested-or-going.ts
+++ src/pages/interested-or-going/interested-or-going.ts
@@ -8,30 +8,59 @@ import firebase from 'firebase';
   templateUrl: 'interested-or-going.html',
 })
 export class InterestedOrGoingPage {
-  usersKeys;
+  usersKeys = [];
   users = [];
   type;
+  loader: any = '';
+  loadingText: string;
   usersdata = firebase.database().ref('/users');
+
   constructor(private navParams: NavParams) {}
 
   ionViewWillEnter() {
     this.usersKeys = this.navParams.get('users');
     this.type = this.navParams.get('type');
     this.fetchUsers();
+    this.initializeLoader();
+  }
+
+  initializeLoader() {
+    this.loader = this.users.length >= this.usersKeys.length ? 'false' : '';
+    this.loadingText =
+      this.users.length >= this.usersKeys.length
+        ? 'Completed'
+        : 'Loading more users...';
   }
 
   fetchUsers() {
-    this.usersKeys.forEach(user => {
-      this.usersdata
-        .child(`${user.key}/personalData/`)
-        .once('value', snapshot => {
-          this.users.push(snapshot.val());
-        });
+    return new Promise((resolve, reject) => {
+      for (let i = this.users.length; i < this.users.length + 10; i++) {
+        this.initializeLoader();
+        if (i >= this.usersKeys.length) {
+          break;
+        }
+        this.usersdata
+          .child(`${this.usersKeys[i].key}/personalData`)
+          .once('value', snapshot => {
+            this.users.push(snapshot.val());
+          })
+          .then(() => {
+            resolve(true);
+          })
+          .catch(() => {
+            reject(false);
+          });
+      }
     });
   }
 
   doInfinite(infiniteScroll) {
-    this.fetchUsers();
-    infiniteScroll.complete();
+    this.fetchUsers()
+      .then(() => {
+        infiniteScroll.complete();
+      })
+      .catch(() => {
+        alert('Something went wrong');
+      });
   }
 }
